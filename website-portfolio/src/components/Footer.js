bc22f2f4265e1feeb61784745b0cec89c838c84f
Cinodevv <96893418+Cinodevv@users.noreply.github.com>
2024-09-06 19:38:59 -0400

    bug: Fix white layer and footer
    Fix bug where bottom of the page has a white space.
    Fix footer on small screens.

--- website-portfolio/src/components/Footer.js
+++ website-portfolio/src/components/Footer.js
@@ -1,7 +1,10 @@
 function Footer({ isDarkMode }) {
   return (
     <div className="text-center">
-      <p className={`${isDarkMode ? 'text-White' : 'text-Black'}`}>Created By Armando Mancino. © 2024 All rights reserved.</p>
+      <p className={`${isDarkMode ? 'text-White' : 'text-Black'}`}>
+        Created By Armando Mancino. © 2024{' '}
+        <span className="d-block d-sm-inline">All rights reserved.</span>
+      </p>
     </div>
   );
 }
